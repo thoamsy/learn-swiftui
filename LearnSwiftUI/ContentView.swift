0697dd74f5f1172ccb2ba10186598e17f67488bb
YangKui <c6swift@gmail.com>
2019-06-17 21:39:01 +0800

    Initial Commit

---
+++ LearnSwiftUI/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  LearnSwiftUI
+//
+//  Created by yk on 6/17/19.
+//  Copyright © 2019 yk. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
